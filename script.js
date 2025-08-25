4db27e1bfda8d47d215e27578dbcb39821bd79d5
Arianna R <arirah09@gmail.com>
2025-08-25 15:46:26 -0400

    debugging

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const canvas = document.getElementById("sokobanCanvas");
 const ctx = canvas.getContext("2d");
 
-let x = canvas.width / 4;
-let y = 40;
+let x = 100;
+let y = 100;
 
 let dx = 10;
 let dy = 10;
@@ -11,14 +11,12 @@ function draw() {
 
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.beginPath();
-    ctx.arc(x, y, 10, 0, Math.Pi * 2, true);
-    ctx.fillStyle = "rgb(255 0 0 / 50%)";
+    ctx.arc(x, y, 10, 0, Math.PI * 2, true);
+    ctx.fillStyle = "#1E90FF";
     ctx.fill();
     ctx.closePath();
-
     x += dx;
     y += dy;
-
 }
 
 setInterval(draw, 10);
